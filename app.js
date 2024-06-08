3ccfe0457bd9f928ff4a10f47da51cccfaf106e5
lhrvt <you@example.com>
2024-06-08 17:27:12 +0200

    first commit

--- app.js
+++ app.js
@@ -18,46 +18,115 @@ window.addEventListener('DOMContentLoaded', function () {
         // Créer la lumière
         var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene1);
 
-        // Créer un cube
-        var box = BABYLON.MeshBuilder.CreateBox("box", {}, scene1);
+        // Créer un cub
 
         // Variable pour stocker le modèle de la tortue
         var turtle;
+        BABYLON.Effect.ShadersStore['customVertexShader'] = `
+                precision highp float;
 
+                attribute vec3 position;
+                attribute vec3 normal;
+
+                uniform mat4 worldViewProjection;
+
+                varying vec3 vNormal;
+
+                void main(void) {
+                    // Pass the normal to the fragment shader
+                    vNormal = normal;
+
+                    // Calculate the position of the vertex
+                    gl_Position = worldViewProjection * vec4(position, 1.0);
+                }
+            `;
+            BABYLON.Effect.ShadersStore['customwhiteFragmentShader'] = `
+            precision highp float;
+        
+            // Varyings
+            varying vec3 vNormal;
+            uniform float time;
+        
+            void main(void) {
+        
+                vec3 normal = normalize(vNormal);
+                vec3 lightDir = vec3(0.5* time, 0.5 , 1.0);
+                float intensity = dot(normal, lightDir);
+        
+                if (intensity > 0.6) {
+                    gl_FragColor = vec4(0.2, 0.6, 0.8, 1.0);  // Rose pastel
+                } else if (intensity > 0.4) {
+                    gl_FragColor = vec4(0.4, 0.6, 1.0, 1.0);  // Violet pastel
+        
+                } else {
+                    gl_FragColor = vec4(0.0, 0.0, 0.4, 1.0);  
+                }
+            }
+            `;
+         
+            var normalTexture = new BABYLON.Texture("as_n.exr", scene1);
+            
+            var white_toon = new BABYLON.ShaderMaterial('toonShader', scene1, {
+                vertex: 'custom',
+                fragment: 'customwhite',
+            }, {
+                needAlphaBlending: false,
+                needAlphaTesting: false,
+                attributes: ['position', 'normal', 'uv'],
+                uniforms: ['worldViewProjection', 'time', 'normalMap'],
+        
+            });
+
+            
+            white_toon.setTexture("normalMap", normalTexture);
+        function checkMat(mesh, mat){
+
+            if (mesh.subMeshes) {
+                for (var i = 0; i < mesh.subMeshes.length; i++) { 
+                    var subMesh = mesh.subMeshes[i];
+                    var material = subMesh.getMaterial();
+                    console.log(material.name)
+                    if (material) {
+                        mesh.material = mat
+                    }
+                }
+            } else {
+                console.log("Le mesh n'a pas de sous-meshes avec des matériaux différents.");
+            }
+        }
+        
         // Chargement du modèle importé
         BABYLON.SceneLoader.ImportMesh(null, "./model/", "turtle.glb", scene1, function (meshes, particleSystems, skeletons, animationGroups) {
             // Assigner le premier mesh (la tortue) à la variable turtle
-            turtle = meshes[0];
+            if (meshes.length > 0) {
+                turtle = meshes[0];
+                turtle.rotationQuaternion = null;
+                turtle.receiveShadows = true;
+                turtle.castShadow = true;
+                turtle.scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
 
-            // Désactiver la rotation par quaternion
-            turtle.rotationQuaternion = null;
+                // Remplacer le matériau à l'index 0 par le shader toon
+                
+                
 
-            // Activer la réception et la projection d'ombres
-            turtle.receiveShadows = true;
-            turtle.castShadow = true;
-            
-            turtle.scaling = new BABYLON.Vector3(0.5, 0.5, 0.5);
-            // Stocker les animations dans un tableau
-            var animations = [];
-            animationGroups.forEach(function(animationGroup, index) {
-                animations[index] = animationGroup;
-            });
-        });
-
-        // Fonction de mise à jour avant le rendu
-        scene1.registerBeforeRender(function () {
-            // Faire tourner le cube
-            box.rotation.y += 0.01;
-
-            if (turtle) {
-                turtle.rotation.y += 0.01;
+                var animations = [];
+                animationGroups.forEach(function(animationGroup, index) {
+                    animations[index] = animationGroup;
+                });
+            } else {
+                console.error("Aucun mesh n'a été trouvé dans le modèle.");
             }
+            scene1.registerBeforeRender(function () {
+                if (turtle) {
+                    turtle.rotation.y += 0.01;
+                }
+            });
            
         });
 
         return scene1;
     };
-
+    
     // Créer la première scène
     var scene1 = createScene1();
 
